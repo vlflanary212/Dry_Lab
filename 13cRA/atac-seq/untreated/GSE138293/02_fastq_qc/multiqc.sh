5e86673eef34787f00db54e031ba47b0d0043b0b
Victoria L. Flanary <85571685+vlflanary212@users.noreply.github.com>
2025-01-20 21:04:20 -0600

    add atac-seq preprocessing

---
+++ 13cRA/atac-seq/untreated/GSE138293/02_fastq_qc/multiqc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+##### SLURM #####
+#SBATCH --job-name=multiqc
+#SBATCH --partition=express
+#SBATCH --time=01:59:59
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4G
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-user=flanary@uab.edu
+#SBATCH --mail-type=ALL
+
+##### VARIABLES #####
+wd="/home/flanary/Dry_Lab/13cRA/atac-seq/untreated/GSE138293/02_fastq_qc"
+fastqc_output=$wd/"fastqc_output"
+multiqc_output=$wd/"multiqc_output"
+
+##### PACKAGES #####
+module load MultiQC/0.8
+
+##### COMMANDS #####
+multiqc -v $fastqc_output -o $multiqc_output
+
+##### END #####
+echo "done"
