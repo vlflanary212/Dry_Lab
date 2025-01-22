7e63602325be0a3ba03b90edb7dc25aa847e48ec
Victoria L. Flanary <85571685+vlflanary212@users.noreply.github.com>
2025-01-21 20:33:46 -0600

    post-alignment processing

---
+++ 13cRA/atac-seq/untreated/GSE138293/06_format_bam/01_rename_bam.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+##### VARIABLES #####
+bam_dir="/data/scratch/flanary/atac-seq/untreated/GSE138293/bam"
+mapping="/home/flanary/Dry_Lab/13cRA/atac-seq/untreated/GSE138293/06_format_bam/bam_filename_mapping.txt"
+
+# Enter the bam directory
+cd "$bam_dir"
+
+# Read the mapping file line by line
+while IFS=$'\t' read -r old_prefix new_prefix; do
+    # Define the old and new filenames
+    old_bam="${old_prefix}_mapped.bam"
+    new_bam="${new_prefix}_mapped.bam"
+
+    # Rename the file
+    mv "$old_bam" "$new_bam"
+    echo "Renamed $old_bam to $new_bam"
+done < "$mapping"
+
+echo "Renaming complete!"
